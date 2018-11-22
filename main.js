f1f6835895ca45654c08817af3726cd6d8e8e00c
Tim Ruhay <timruhay@gmail.com>
2018-11-23 06:10:37 +0700

    fixed Package scripts

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow() {
 
   // and load the index.html of the app.
   // mainWindow.loadFile('index.html')
-  mainWindow.loadURL('http://127.0.0.1:3434');
+  mainWindow.loadURL('http://127.0.0.1:3301');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
