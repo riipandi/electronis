5e58727397d8f03e5e9e041e2ef5220546f0f1c5
Tim Ruhay <timruhay@gmail.com>
2018-11-23 05:54:25 +0700

    Added installer script

---
+++ setup/win32.js
@@ -0,0 +1,22 @@
+//
+// https://www.christianengvall.se/electron-windows-installer/
+//
+
+var electronInstaller = require('electron-winstaller');
+
+result = electronInstaller.createWindowsInstaller({
+  appDirectory: '../build/Electronis-win32-x64',
+  outputDirectory: '../dist',
+  title: 'Electronis Installer',
+  authors: 'Aris Ripandi',
+  exe: 'electronis.exe'
+});
+
+result.then(
+  () => {
+    console.log('The installers of your application were succesfully created!');
+  },
+  e => {
+    console.log(`Well, sometimes you are not so lucky: ${e.message}`);
+  }
+);
